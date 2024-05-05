b557d42e05e5cfd24657ecb9943e64996f925a9a
oskar <naturalmen2017@gmail.com>
2024-05-05 18:31:39 +0800

    Начал добавлять часы

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -44,19 +44,45 @@ namespace ЭВМ
             ResistText.Text = "0,000";
             GndButton.Opacity= 0;
             UsbButton.Opacity= 0;
+            RtcButton.Opacity= 0;   
             Plus.IsHitTestVisible = true;
             Minus.IsHitTestVisible = true;
         }
 
         private void update(object sender, EventArgs e)
         {
-            if ((AorusB450 != null) &&(rand.Next(0,100)>95))
+            if ((AorusB450 != null) &&(rand.Next(0,100)>95)) //compare[0] - это минус,compare[1] - плюс.От плюса отнимаем минус compare[1]-compare[0]
             {
+                if (compare[0].isGND || compare[1].isGND) //если мерим м/у gnd и элементом
+                {
+                    VoltageText.Text = "" + ((float)Convert.ToDouble(compare[1].V) - (float)Convert.ToDouble(compare[0].V));
+                    if (compare[0].isGND)
+                    {
+                        AmperText.Text = compare[1].A;
+                        ResistText.Text = compare[1].R;
+                    }
+                    else
+                    {
+                        AmperText.Text = compare[0].A;
+                        ResistText.Text = compare[0].R;
+                    }
+                }
+                else 
+                {
+                    AmperText.Text = "???";
+                    VoltageText.Text = "???";
+                    ResistText.Text = "???";
+                }
                 AorusB450.refresh(1);
+                /*AorusB450.usb.V;*/
+                /*VoltageText.Text = "" + ((float)Convert.ToDouble(compare[1].V) - (float)Convert.ToDouble(compare[0].V));
+                AmperText.Text = "" + Math.Abs((float)Convert.ToDouble(compare[1].A) - (float)Convert.ToDouble(compare[0].A));*/
+                    /*AmperText.Text = AorusB450.usb.A;*/
+                    //если мерим между двумя элементами то сила тока считается через мощность бп
+               /* AmperText.Text = AorusB450.usb.A;
                 VoltageText.Text = AorusB450.usb.V;
-                AmperText.Text = AorusB450.usb.A;
-                ResistText.Text = AorusB450.usb.R;
-                /*string roundedNumber = number.ToString("N3");*/
+                ResistText.Text = AorusB450.usb.R;*/
+               /*string roundedNumber = number.ToString("N3");*/
             }
         }
 
@@ -69,14 +95,58 @@ namespace ЭВМ
             }
         }
 
-   
-        private void GndClick(object sender, RoutedEventArgs e)//gnd всегда в compare[0],второй элемент всегда в compare[1]
+        private void Rtc_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)Minus.IsChecked)//обработка выделения GND. compare[0] - это минус,compare[1] - плюс
+            {
+                RtcButton.Opacity = 1;
+                Minus.IsChecked = false;
+                Plus.IsChecked = false;
+                compare[0] = AorusB450.rtc;
+                if (GetNumOfElem(compare) == 2)
+                {
+                    if (compare[0] != compare[1])
+                    {
+                        CompositionTarget.Rendering += update;
+                        Plus.IsHitTestVisible = false;
+                        Minus.IsHitTestVisible = false;
+                        return;
+                    }
+                    Array.Clear(compare, 1, compare.Length);
+                }
+            }
+            else if ((bool)Plus.IsChecked)//compare[0] - это минус,compare[1] - плюс
+            {
+                RtcButton.Opacity = 1;
+                Minus.IsChecked = false;
+                Plus.IsChecked = false;
+                compare[1] = AorusB450.rtc;
+                if (GetNumOfElem(compare) == 2)
+                {
+                    if (compare[0] != compare[1])
+                    {
+                        CompositionTarget.Rendering += update;
+                        Plus.IsHitTestVisible = false;
+                        Minus.IsHitTestVisible = false;
+                        return;
+                    }
+                    Array.Clear(compare, 1, compare.Length);
+                }
+            }
+        }
+
+
+
+
+
+        private void GndClick(object sender, RoutedEventArgs e)
         {
             
-            if ((bool)Minus.IsChecked)//обработка выделения GND
+            if ((bool)Minus.IsChecked)//обработка выделения GND. compare[0] - это минус,compare[1] - плюс
             {
                 GndButton.Opacity = 1;
                 Minus.IsChecked = false;
+                Plus.IsChecked= false;
                 compare[0]=AorusB450.gnd;   
                 if (GetNumOfElem(compare) == 2) 
                 {
@@ -89,17 +159,34 @@ namespace ЭВМ
                     }
                     Array.Clear(compare, 1, compare.Length);
                 }
-                //добавил в массив объект
-                //запуски функции
+            }
+            else if ((bool)Plus.IsChecked)//compare[0] - это минус,compare[1] - плюс
+            {
+                GndButton.Opacity = 1;
+                Minus.IsChecked = false;
+                Plus.IsChecked = false;
+                compare[1] = AorusB450.gnd;
+                if (GetNumOfElem(compare) == 2)
+                {
+                    if (compare[0] != compare[1])
+                    {
+                        CompositionTarget.Rendering += update;
+                        Plus.IsHitTestVisible = false;
+                        Minus.IsHitTestVisible = false;
+                        return;
+                    }
+                    Array.Clear(compare, 1, compare.Length);
+                }
             }
         }
 
-        private void UsbClick(object sender, RoutedEventArgs e)//обработка всех областей
+        private void UsbClick(object sender, RoutedEventArgs e)//compare[0] - это минус,compare[1] - плюс
         {
 
-            if ((bool)Plus.IsChecked)//обработка выделения GND
+            if ((bool)Plus.IsChecked)//compare[0] - это минус,compare[1] - плюс
             {
                 UsbButton.Opacity = 1;
+                Minus.IsChecked = false;
                 Plus.IsChecked = false;
                 compare[1] = AorusB450.usb;
                 if (GetNumOfElem(compare) == 2) 
@@ -114,6 +201,26 @@ namespace ЭВМ
                     Array.Clear(compare, 1, compare.Length);
                 }
             }
+            else if ((bool)Minus.IsChecked)//compare[0] - это минус,compare[1] - плюс
+            {
+                UsbButton.Opacity = 1;
+                Minus.IsChecked = false;
+                Plus.IsChecked = false;
+                compare[0] = AorusB450.usb;
+                if (GetNumOfElem(compare) == 2)
+                {
+                    if (compare[0] != compare[1])
+                    {
+                        CompositionTarget.Rendering += update;
+                        Plus.IsHitTestVisible = false;
+                        Minus.IsHitTestVisible = false;
+                        return;
+                    }
+                    Array.Clear(compare, 1, compare.Length);
+                }
+            }
+
+
         }
 
         public void Tab_Click(object sender, RoutedEventArgs e)
@@ -179,17 +286,18 @@ namespace ЭВМ
 
         private void Plus_Click(object sender, RoutedEventArgs e)
         {
-            Minus.IsChecked = false;
+            /*Minus.IsChecked = false;*/
         }
 
         private void Minus_Click(object sender, RoutedEventArgs e)
         {
-            Plus.IsChecked = false;
+           /* Plus.IsChecked = false;*/
         }
 
 
         public class MotherBoard
         {
+            public int W;//мощность блока питания
             private Random rnd;
             public Elem rtc;
             public Elem gnd;
@@ -201,6 +309,7 @@ namespace ЭВМ
                 {                      //от 0.450мВ до 0.7мВ
                     case 1://всё исправно
                         usb.Fill(usb.V.Substring(0,3) + rnd.Next(10, 100), (""+((float)Convert.ToDouble(usb.V)* Math.Pow(10,3) / ((float)Convert.ToDouble(usb.A)*Math.Pow(10,3)))).Substring(0,4), "0," + 5);//v,r,a
+                        gnd.Fill("0", "0", "0");
                         /*Trace.WriteLine(usb.R);*/
                         /*Trace.WriteLine(((float)Convert.ToDouble(usb.V) * Math.Pow(10, -3) / (float)Convert.ToDouble(usb.A)));
                         Trace.WriteLine(usb.R);*/
@@ -215,14 +324,15 @@ namespace ЭВМ
             {
                 rnd = new Random();
                 rtc = new Elem();
-                gnd = new Elem();
                 usb = new Elem();
+                gnd = new Elem(true);
+                W = 500;
                 switch (branching)
                 {          //от 0.450мВ до 0.7мВ
                     case 1://всё исправно
                         usb.Fill("0,"+rnd.Next(4,7)+rnd.Next(10,100), "1", "0," + 5);//v,r,a
-                        /*MainWindow.VoltageText*/
-                        /*NumbersVoltage.IsChecked = false;*/
+                        gnd.Fill("0", "0", "0");
+                        /*rtc.Fill();*/
                         break;
                     case 2:////часы не работают - не работает южный порт
                            //осцилограма не показывает//график не синусоидальный//Частота не 32768Гц
@@ -233,18 +343,28 @@ namespace ЭВМ
 
         }
 
+        private void RtcButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 
 }
 
     public class Elem
     {
+        public bool isGND=false;
         public string V;//напряжение
         public string R;//сопротивление
         public string A;//сила тока
+        public Elem(bool GND)
+        {
+        V = ""; R=""; A=""; isGND = GND;
+        }
+
         public Elem()
         {
-        V = ""; R=""; A="";
+            V = ""; R = ""; A = "";
         }
         public void Fill(string v, string r, string a)
         {
@@ -253,6 +373,7 @@ namespace ЭВМ
             A = a;//сила тока
         }
     }
+
 
     public static class Tools //класс для метода инициализации
     {
@@ -271,11 +392,21 @@ namespace ЭВМ
         public static int GetNumOfElem(Elem[] arr)
         {
         int k = 0;
-        for (int i = 0; i < arr.Length;i++)
-        {
-        if (arr[i] != null) k++;
-        }
+            for (int i = 0; i < arr.Length;i++)
+            {
+            if (arr[i] != null) k++;
+            }
         return k;
+        }
+        
+        public static void HiddenButtons(Elem[] arr)
+        {
+            for (int i = 0;i < arr.Length;i++) 
+            {
+
+
+            }
+
         }
 
     }
